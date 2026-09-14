68e1dae64f2b22f342d4113f4382743135ed1dec
r-gt <thiagoobregon000@gmail.com>
2026-09-14 09:31:29 -0300

    edited main.c

--- main.c
+++ main.c
@@ -14,18 +14,36 @@
 int main(int argc, char **argv){
 
 	SDL_Window *window;
-	SDL_Renderer * renderer;
+	Clay_SDL3RendererData * renderer;
 
-	SDL_CreateWindowAndRenderer("Delta", 640, 480, 0, &window, &renderer);
+	SDL_CreateWindowAndRenderer("Delta", 640, 480, 0, &window, &renderer->renderer);
+
+
+	uint64_t totalMemorySize = Clay_MinMemorySize();
+	Clay_Arena clayMemory = (Clay_Arena) {
+		.memory = SDL_malloc(totalMemorySize),
+		.capacity = totalMemorySize
+	};
+
+
 
 	_Bool running=true;
 	while(running){
 
-		SDL_Event event;
-		while(SDL_PollEvent(&event)) {
+		SDL_Event * event;
+		while(SDL_PollEvent(event)) {
 
-			if (event.type == SDL_EVENT_QUIT || event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) running = false;
+			switch(event->type){
 
+				case(SDL_EVENT_QUIT || SDL_EVENT_WINDOW_CLOSE_REQUESTED): running = false; break;
+
+				case SDL_EVENT_WINDOW_RESIZED:
+					Clay_SetLayoutDimensions((Clay_Dimensions) { (float) event->window.data1, (float) event->window.data2 });
+					break;
+				case SDL_EVENT_MOUSE_WHEEL:
+					Clay_UpdateScrollContainers(true, (Clay_Vector2) { event->wheel.x, event->wheel.y }, 0.01f);
+					break;
+			}
 		}
 
 	}
